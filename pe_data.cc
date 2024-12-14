8a59974fedf87c502ce9d3d97d61e16756facada
Victor Ariño Pellejero <142635375+VectorPointer@users.noreply.github.com>
2024-12-14 19:25:37 +0100

    Update pe_data.cc

--- pe_data.cc
+++ pe_data.cc
@@ -12,10 +12,10 @@ double insertion_sort(vector<int> &v) {
     int val;
     clock_t start = clock();
 
-    for(int i = 1; i < v_size; ++i) {
+    for (int i = 1; i < v_size; ++i) {
         val = v[i];
         int j = i;
-        while(j > 0 and v[j - 1] > val) {
+        while (j > 0 and v[j - 1] > val) {
             v[j] = v[j - 1];
             --j;
         }
@@ -32,9 +32,9 @@ double selection_sort(vector<int> &v) {
     int pos;
     clock_t start = clock();
 
-    for(int i = 0; i < v_size; ++i) {
+    for (int i = 0; i < v_size; ++i) {
         pos = i;
-        for(int j = i + 1; j < v_size; ++j) if(v[pos] > v[j]) pos = j;
+        for (int j = i + 1; j < v_size; ++j) if (v[pos] > v[j]) pos = j;
         swap(v[i],v[pos]);
     }
     clock_t end = clock();
@@ -51,7 +51,7 @@ int main() {
     }
     file << "Tamaño_Vector,Tiempo_Insertion,Tiempo_Selection\n";
 
-    for(int i = 0; i < 50; ++i) {
+    for (int i = 0; i < 50; ++i) {
         double insertion_time = 0;
         double selection_time = 0;
 
@@ -59,14 +59,14 @@ int main() {
         int v_size = rand()%4001 + 1000;
         vector<int> v(v_size);
         vector<int> v_aux(v_size);
-        for(int j = 0; j < v_size; ++j) {
+        for (int j = 0; j < v_size; ++j) {
             // Cada valor de vector és 0 <= v[j] <= 10^6
-            v[j] = v_aux[j] = (rand()%int(pow(10,6)+1));
+            v[j] = v_aux[j] = (rand()%int(pow(10, 6) + 1));
         }
         
         // L'ordre d'execució dels programes es totalment aleatori
         bool ordre = rand()%2;
-        if(ordre) {
+        if (ordre) {
             insertion_time = insertion_sort(v);
             selection_time = selection_sort(v_aux);
         }
